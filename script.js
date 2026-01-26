56963b0c0f91233128133009f203363ddf035cbd
BaslielZ <baslielz727@gmail.com>
2026-01-26 14:14:20 +0200

    Exercise 5 and 6

--- script.js
+++ script.js
@@ -16,11 +16,51 @@ function crossoff(id){
 
 button = document.getElementById('addItem').addEventListener('click', addItem)
 
-num = 5
+num = 1
+todolist = []
 function addItem() {
     item = document.getElementById('newItem').value
-    document.getElementById('list').innerHTML += ` <li class="normal" id="item${num++}" onclick="crossoff('item${num-1}')">${item} <button onclick="removeItem('item${num-1}')">delete</button></li>`
+    if (item != ""){
+        if (document.getElementById('high').checked == true){
+            obj = {text: item, priority: 'high'}
+            todolist.push(obj)
+        }
+        else if (document.getElementById('mid').checked == true) {
+            obj = {text: item, priority: 'mid'}
+            todolist.push(obj)
+        }
+        else{
+            obj = {text: item, priority: 'low'}
+            todolist.push(obj)
+        }
+        renderList()
+        document.getElementById('high').checked = false
+        document.getElementById('mid').checked = false
+        document.getElementById('low').checked = false
+    }
+    
 }
+
+function renderList(){
+    document.getElementById('list').innerHTML = ""
+    for (i=0; i<todolist.length;i++){
+        if (todolist[i].priority == 'high'){
+            document.getElementById('list').innerHTML += ` <li class="${todolist[i].priority}" id="item${num++}" onclick="crossoff('item${num-1}')">${todolist[i].text} <button onclick="removeItem('item${num-1}')">delete</button></li>`
+        }
+    }
+    for (i=0; i<todolist.length;i++){
+        if (todolist[i].priority == 'mid'){
+            document.getElementById('list').innerHTML += ` <li class="${todolist[i].priority}" id="item${num++}" onclick="crossoff('item${num-1}')">${todolist[i].text} <button onclick="removeItem('item${num-1}')">delete</button></li>`
+        }
+    }
+    for (i=0; i<todolist.length;i++){
+        if (todolist[i].priority == 'low'){
+            document.getElementById('list').innerHTML += ` <li class="${todolist[i].priority}" id="item${num++}" onclick="crossoff('item${num-1}')">${todolist[i].text} <button onclick="removeItem('item${num-1}')">delete</button></li>`
+        }
+    }
+}
+    
+
 
 
 function removeItem(id) {
